ab8588d93d409ef6b4d96c708fe0e4eff72f1968
AlexSh <alex@shulg.in>
2020-11-12 23:03:14 +0500

    programms

--- components/Teasers.vue
+++ components/Teasers.vue
@@ -18,7 +18,9 @@
         dlink="/_nuxt/docs/Стажировка.pdf"
         >
         <!-- <template v-slot:overline>Программа 1</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Реализация программ непрерывного образования для специалистов секторов экономики, значимых для региона (в форме стажировки) (32 
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
@@ -27,10 +29,12 @@
       <teaser-column :rows="2" 
         action="ЗАРЕГИСТРИРОВАТЬСЯ"
         plink="https://catdo.susu.ru/checkout/?add-to-cart=35390"
-        dlink="/_nuxt/docs/Стажировка.pdf"
+        dlink="/_nuxt/docs/Цифровые компетенции в профессиональной пед деятельности.pdf"
         >
         <!-- <template v-slot:overline>Программа 2</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Цифровые компетенции в профессиональной педагогической деятельности (72 час., грант, 2020) 
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
@@ -39,10 +43,12 @@
       <teaser-column :rows="2" 
         action="ЗАРЕГИСТРИРОВАТЬСЯ"
         plink="https://catdo.susu.ru/checkout/?add-to-cart=35389"
-        dlink="/_nuxt/docs/Стажировка.pdf"
+        dlink="/_nuxt/docs/Наставничество.pdf"
         >
         <!-- <template v-slot:overline>Программа 3</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Наставничество в системе непрерывного образования взрослых (72 час., грант, 2020)
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
@@ -51,10 +57,12 @@
       <teaser-column :rows="2" 
         action="ЗАРЕГИСТРИРОВАТЬСЯ"
         plink="https://catdo.susu.ru/checkout/?add-to-cart=35388"
-        dlink="/_nuxt/docs/Стажировка.pdf"
+        dlink="/_nuxt/docs/Цифровая трансформация.pdf"
         >
         <!-- <template v-slot:overline>Программа 3</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Цифровая трансформация дополнительного профессионального образования (72 час., грант, 2020)
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
@@ -63,10 +71,12 @@
       <teaser-column :rows="2" 
         action="ЗАРЕГИСТРИРОВАТЬСЯ"
         plink="https://catdo.susu.ru/checkout/?add-to-cart=35387"
-        dlink="/_nuxt/docs/Стажировка.pdf"
+        dlink="/_nuxt/docs/Управление персоналом.pdf"
         >
         <!-- <template v-slot:overline>Программа 3</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Управление персоналом организации, реализующей дополнительные профессиональные программы (72 час., грант, 2020) 
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
@@ -75,10 +85,12 @@
       <teaser-column :rows="2" 
         action="ЗАРЕГИСТРИРОВАТЬСЯ"
         plink="https://catdo.susu.ru/checkout/?add-to-cart=35386"
-        dlink="/_nuxt/docs/Стажировка.pdf"
+        dlink="/_nuxt/docs/Модели и технологии образования взрослых.pdf"
         >
         <!-- <template v-slot:overline>Программа 3</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Модели и технологии образования взрослых (72 час., грант, 2020) 
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
@@ -87,10 +99,12 @@
       <teaser-column :rows="2" 
         action="ЗАРЕГИСТРИРОВАТЬСЯ"
         plink="https://catdo.susu.ru/checkout/?add-to-cart=35385"
-        dlink="/_nuxt/docs/Стажировка.pdf"
+        dlink="/_nuxt/docs/ОВЗ.pdf"
         >
         <!-- <template v-slot:overline>Программа 3</template> -->
-        <template v-slot:headline>кратное описание</template>
+        <template v-slot:headline>
+          Разработка и реализация дополнительных профессиональных программ для инвалидов и лиц с ограниченными возможностями здоровья (72 
+        </template>
         <template v-slot:content
           >кратное описание, consectetur adipiscing elit. Aliquam at
           ipsum eu nunc commodo posuere et sit amet ligula.</template
